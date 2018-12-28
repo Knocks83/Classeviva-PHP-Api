67be2accb0b7e9614022636b174445cc13b1bd72
Luca Fenu <22529815+Knocks83@users.noreply.github.com>
2018-12-28 13:26:16 +0100

    Added Notes and Schoolbooks
    Fixed agenda and calendar
    Request method automatically checks if it has to get or post

--- Classeviva.php
+++ Classeviva.php
@@ -3,11 +3,17 @@ class Classeviva {
     private $baseUrl = 'https://web.spaggiari.eu/rest/v1';
 
     private function Request ($dir, $data = []) {
-        print ($this->baseUrl.$dir);
+        if ($data == []) {
+            curl_setopt($this->curl, CURLOPT_POST, false);
+        } else {
+            curl_setopt_array($this->curl, [
+                CURLOPT_POST       => true,
+                CURLOPT_POSTFIELDS => $data,
+            ]);
+            
+        }
         curl_setopt_array ($this->curl, [
-            CURLOPT_POST       => true,
             CURLOPT_URL        => $this->baseUrl.$dir,
-            CURLOPT_POSTFIELDS => $data,
         ]);
 
         return curl_exec ($this->curl);
@@ -50,6 +56,7 @@ class Classeviva {
             $this->firstName = $response->firstName;
             $this->lastName = $response->lastName;
             $this->token = $response->token;
+            $this->id = filter_var($response->ident, FILTER_SANITIZE_NUMBER_INT);
             curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Z-Dev-Apikey: +zorro+',
@@ -63,15 +70,19 @@ class Classeviva {
     }
 
     public function agenda ($begin, $end, $events = 'all') {
-        return $this->Request("/students/$this->ident/$events/$begin/$end");
+        return $this->Request("/students/$this->id/agenda/$events/$begin/$end");
     }
 
     public function calendar () {
-        return $this->Request("/students/$this->ident/calendar/all");
+        return $this->Request("/students/$this->id/calendar/all");
     }
 
     public function notes () {
-        return $this->Request("/students/$this->ident/notes/all");
+        return $this->Request("/students/$this->id/notes/all");
+    }
+
+    public function schoolbooks () {
+        return $this->Request("/students/$this->id/schoolbooks");
     }
 }
 
